a09bd248dbd860c0182ce0448d402463a7dec877
Asad <asadjr29@gmail.com>
2025-12-05 10:46:21 +0600

    5-12

---
+++ array/insert_at_last.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    //indexing from 1 to n
+
+   int size = sizeof(arr)/sizeof(arr[0]);
+
+    
+    arr[size] = 6;
+    size++;
+
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
