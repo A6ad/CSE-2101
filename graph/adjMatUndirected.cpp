6a8c5b6504953211b79ad051418fa462954016dc
Asad <asadjr29@gmail.com>
2026-04-02 01:01:34 +0600

    graph

---
+++ graph/adjMatUndirected.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {                      // shortcut
+            adj_mat[i][j] = 0; // memset(adj_mat,0,sizeof(adj_mat))
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < e; j++)
+        {
+            if (i == j)
+            {
+                adj_mat[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_mat[a][b] = 1;
+        adj_mat[b][a] = 1; // graph is undirected
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
